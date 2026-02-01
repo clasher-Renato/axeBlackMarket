e083d70df20996c3c5a9ea711743c743a19b0d7c
Renato Machado <81891866+Machadoo7@users.noreply.github.com>
2026-02-01 20:17:39 +0000

    project setup

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version("cerulean")
+game("gta5")
+lua54("yes")
+author("AXE Scripts")
+description("BlackMarket Script")
+version("1.0.0")
+
+shared_scripts({
+	"config.lua",
+})
+
+client_scripts({
+	"client/cl-main.lua",
+})
+
+server_scripts({
+	"server/sv-main.lua",
+})
