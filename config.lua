f7d885203561e19be01f8328fa9d8cda7316c31d
Renato Machado <81891866+Machadoo7@users.noreply.github.com>
2026-02-01 20:24:47 +0000

    chore(config): add initial config file

--- config.lua
+++ config.lua
@@ -1,1 +1,18 @@
 Config = {}
+
+Config.BlackMarket = {
+	label = "BlackMarket",
+	ped = `g_m_importexport_01`,
+	interactText = "Open BlackMarket",
+}
+
+Config.Locations = {
+	{
+		coords = vector4(145.96, -1058.95, 30.19, 64.87),
+		tip = "Find me behind the city bank.",
+	},
+	{
+		coords = vector4(-102.4, 6345.04, 31.58, 212.4),
+		tip = "Find me in the paleto motel.",
+	},
+}
